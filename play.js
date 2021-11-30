ab3d852368fc759ee985926c80e495e16fb2ed04
GHadeer Khader <qods_a@yahoo.com>
2021-11-30 18:41:37 +0000

    exporting connect to client file

--- play.js
+++ play.js
@@ -1,20 +1,25 @@
 const net = require("net");
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "localhost",
-    port: 50541,
-  });
- 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-  
-  conn.on("connect", () => {
-    console.log("you ded cuz you idled");
-  });
-  return conn;
-};
+const connect = require('./client');
 
-console.log("Connecting ...");
-connect();
+// establishes a connection with the game server
+// const connect = function () {
+//   const conn = net.createConnection({
+//     host: "localhost",
+//     port: 50541,
+//   });
+ 
+//   // interpret incoming data as text
+//   conn.setEncoding("utf8");
+  
+//   conn.on("connect", () => {
+//     console.log("you ded cuz you idled");
+//   });
+//   return conn;
+// };
+
+// console.log("Connecting ...");
+// connect();
+
+
+module.exports = connect;
