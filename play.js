c328a5053ccdd147bda544e5b173c53c70974aa0
GHadeer Khader <qods_a@yahoo.com>
2021-12-01 01:06:44 +0000

    modify input

--- play.js
+++ play.js
@@ -2,6 +2,9 @@
 
 const connect = require('./client');
 
+const connect = require('./input');
+
+
 
 // establishes a connection with the game server
 // const connect = function () {
@@ -19,21 +22,21 @@ const connect = require('./client');
 //   return conn;
 // };
 
-const handleUserInput = function () {
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
+// const handleUserInput = function () {
+//   if (key === '\u0003') {
+//     process.exit();
+//   }
+// };
 
 
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", (handleUserInput));
-  return stdin;
-};
+// const setupInput = function () {
+//   const stdin = process.stdin;
+//   stdin.setRawMode(true);
+//   stdin.setEncoding("utf8");
+//   stdin.resume();
+//   stdin.on("data", (handleUserInput));
+//   return stdin;
+// };
 
 
  console.log("Connecting ...");
