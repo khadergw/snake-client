6d8958d7e840691dbaf6e2f636bc26f33cea5df0
GHadeer Khader <qods_a@yahoo.com>
2021-12-01 00:58:34 +0000

    event handler

--- play.js
+++ play.js
@@ -19,9 +19,26 @@ const connect = require('./client');
 //   return conn;
 // };
 
+const handleUserInput = function () {
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", (handleUserInput));
+  return stdin;
+};
+
+
  console.log("Connecting ...");
  connect();
-
+setupInput();
 
 
 //module.exports = connect;
